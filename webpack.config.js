c72ebbfbbdd6776100e1543cc4d647a0f71f7607
Arturo Riveron <arthur.borodovisina@gmail.com>
2020-08-28 22:29:01 -0500

    First commint

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  output: {
+    path: __dirname+'/static',
+    filename: "[name].[chunkhash:8].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
